8c5422ff3b4682271390c047f3b404b728dae0d7
Pavel <owner.mad.epa@gmail.com>
2011-08-08 22:15:35 +0000

    starting implement friend list

---
+++ ui.js
@@ -0,0 +1,67 @@
+function makeScrollable(wrapper, scrollable) {
+    var wrapper = $(wrapper),
+        scrollable = $(scrollable);
+
+    scrollable.hide();
+    var loading = $("<div class='loading'></div>").appendTo(wrapper);
+
+    var interval = setInterval(function() {
+        var images = scrollable.find("img");
+        var completed = 0;
+
+        images.each(function() {
+            if (this.complete) completed++;
+        });
+
+        if (completed == images.length) {
+            clearInterval(interval);
+            setTimeout(function() {
+
+                loading.hide();
+                wrapper.css({
+                    overflow: "hidden"
+                });
+
+                scrollable.slideDown("slow", function() {
+                    enable();
+                });
+            }, 1000);
+        }
+    }, 100);
+
+    function enable() {
+        var inactiveMargin = 100;
+
+        var wrapperWidth = wrapper.width();
+        var wrapperHeight = wrapper.height();
+
+        var scrollableHeight = scrollable.outerHeight() + 2 * inactiveMargin;
+
+        wrapper.mousemove(function(e) {
+            var wrapperOffset = wrapper.offset();
+            var top = (e.pageY - wrapperOffset.top) * (scrollableHeight - wrapperHeight) / wrapperHeight - inactiveMargin;
+
+            if (top < 0) {
+                top = 0;
+            }
+
+            wrapper.scrollTop(top);
+        });
+    }
+}
+
+function fill_friend_list(friend_list) {
+    $('.contact').remove();
+    
+    friend_list.forEach(function(e) {
+        $('div.sc_menu').append("<a class='contact' href='#' onclick='peacock(" + e.uid 
+                                + ", 1);'><img class='contact_photo fl_l' src='" 
+                                + e.photo + "'/><span class='contact_name fl_l'>" 
+                                + e.first_name + " " 
+                                + e.last_name + "</span></a>");
+    });
+}
+
+$(function() {
+    makeScrollable("div.sc_menu_wrapper", "div.sc_menu");
+});
